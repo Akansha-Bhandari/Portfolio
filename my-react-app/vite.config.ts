0cb6b3e4eb68c0585b589928266649566b309e37
Akansha-Bhandari <56572942+Akansha-Bhandari@users.noreply.github.com>
2025-03-31 22:24:12 +0530

    Update

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',  // Matches your GitHub repo name
+  build: {
+    outDir: 'dist',     // Explicit output folder
+    emptyOutDir: true,  // Clears the dist folder on each build
+  }
 })
